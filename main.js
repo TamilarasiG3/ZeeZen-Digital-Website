e6a5a5d97ccc98218e4e7d31c9bbbec65849fdd5
TamilarasiG3 <your-email@example.com>
2026-06-27 15:37:35 +0530

    commit

---
+++ main.js
@@ -0,0 +1,13 @@
+document
+.getElementById("contactForm")
+.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+alert(
+"Thank you for contacting ZeeZen Digital. We will get back to you soon."
+);
+
+this.reset();
+
+});
